31e107159bb11c11f4895baca57eeb1aaf5d5fa9
Fabien Basmaison <fabien@arkhi.org>
2015-02-13 13:10:47 +0800

    * Change a class to take assignments into consideration when applying styles. * Add .back-to-top and icons tweaks.

--- classrooms.w3devcampus.user.js
+++ classrooms.w3devcampus.user.js
@@ -19,7 +19,7 @@ if (typeof GM_addStyle === 'undefined') {
 }
 
 GM_addStyle(' \
-  .path-mod-book h2 {  \
+  .pagelayout-incourse h2 {  \
     margin: 6rem 0 .5rem;  \
     font-size: 1.8rem;  \
     font-variant: small-caps; \
@@ -27,27 +27,27 @@ GM_addStyle(' \
     color: #a00;  \
   } \
 \
-  .path-mod-book h3 {  \
+  .pagelayout-incourse h3 {  \
     margin: 4rem 0 .5rem;  \
     font-size: 1.3rem;  \
     color: #090;  \
   } \
 \
-  .path-mod-book h4 {  \
+  .pagelayout-incourse h4 {  \
     margin: 2rem 0 .5rem;  \
     font-size: 1rem;  \
     color: #00a;  \
   } \
 \
-  .path-mod-book h2 + h3 {  \
+  .pagelayout-incourse h2 + h3 {  \
     margin-top: 1rem;  \
   } \
 \
-  .path-mod-book h3 + h4 {  \
+  .pagelayout-incourse h3 + h4 {  \
     margin-top: 1rem;  \
   } \
 \
-  .path-mod-book li {  \
+  .pagelayout-incourse li {  \
     line-height: 1.25;  \
   } \
 \
@@ -55,31 +55,31 @@ GM_addStyle(' \
     font-family: helvetica !important; \
   } \
 \
-  .path-mod-book #page-header {  \
+  .pagelayout-incourse #page-header {  \
     display: none;  \
   } \
 \
-  .path-mod-book .navbar,  \
-  .path-mod-book #page-navbar, \
-  .path-mod-book #block-region-side-pre {  \
+  .pagelayout-incourse .navbar,  \
+  .pagelayout-incourse #page-navbar, \
+  .pagelayout-incourse #block-region-side-pre {  \
     opacity: 0;  \
     transition: opacity .25s; \
   } \
 \
-  .path-mod-book .navbar:hover,  \
-  .path-mod-book #page-navbar:hover, \
-  .path-mod-book #block-region-side-pre:hover {  \
+  .pagelayout-incourse .navbar:hover,  \
+  .pagelayout-incourse #page-navbar:hover, \
+  .pagelayout-incourse #block-region-side-pre:hover {  \
     opacity: 1;  \
   } \
 \
-  .path-mod-book #region-main-essential {  \
+  .pagelayout-incourse #region-main-essential {  \
     width: 33em;  \
     font-size: 1.1rem;  \
     line-height: 1.5 !important;  \
   } \
 \
-  .path-mod-book [class^=rj_insertcode_],  \
-  .path-mod-book [class^=rj_insertcode_] pre { \
+  .pagelayout-incourse [class^=rj_insertcode_],  \
+  .pagelayout-incourse [class^=rj_insertcode_] pre { \
     width: auto !important;  \
     padding: .25em .5em;  \
     border: 0;  \
@@ -89,15 +89,24 @@ GM_addStyle(' \
     background: #e9e9e9;  \
   } \
 \
-  .path-mod-book .block_book_toc .book_toc_numbered {  \
+  .pagelayout-incourse .block_book_toc .book_toc_numbered {  \
     font-size: 1em; \
   } \
 \
-  .path-mod-book .book_content .book_chapter_title { \
+  .pagelayout-incourse .book_content .book_chapter_title { \
     margin-top: 0;  \
     font-size: 2rem; \
     font-weight: bolder !important;  \
     font-variant: initial; \
     text-transform: uppercase;  \
+  } \
+\
+  .back-to-top {  \
+    right: 2em;  \
+    left: auto;   \
+  } \
+\
+  [class^="icon-"]:before, [class*=" icon-"]:before { \
+    text-decoration: none; \
   } \
 ');
